c5261fcebe26c88c153278a36514d44e7abd4aea
gluke0 <milanesigianluca@gmail.com>
2024-01-01 17:35:52 +0100

    centered footer data

--- src/assets/components/footer.vue
+++ src/assets/components/footer.vue
@@ -11,14 +11,19 @@ export default {
   },
 }
 </script>
-
+3
 <template>
   
-  <div>
-    L'Angolo Della Pizza di Mapy
-    Via Cascina Secchi, 516, 
-    24058 Isso BG
-    P. IVA 04604600165
+  <div class="justify-content-center d-flex">
+    <div class="footerino">
+      <div> <b><i>L'Angolo Della Pizza di Mapy </i></b> </div> 
+      
+      <div>  <i> Via Cascina Secchi, 516 </i> </div>
+      
+      <div> <i> 24058 Isso BG </i> </div>
+      
+      <div> P. IVA 04604600165 </div>
+    </div>
   </div>
 
 </template>
@@ -26,4 +31,7 @@ export default {
 <style lang="scss">
 @import '../../style/main.scss';
 
+.footerino{
+  font-size: smaller;
+}
 </style>
