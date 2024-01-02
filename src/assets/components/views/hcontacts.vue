5bbacb5b9683543a01453811ffc1dd0cabc68a62
gluke0 <milanesigianluca@gmail.com>
2024-01-02 17:55:05 +0100

    embedded gmaps

--- src/assets/components/views/hcontacts.vue
+++ src/assets/components/views/hcontacts.vue
@@ -25,7 +25,7 @@ export default {
     </div>
 
     <div>
-      google maps mappa
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2797.3626722527206!2d9.720733277756521!3d45.48264103220566!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4781389911bcafa1%3A0x14b7299ad7ef254!2sL&#39;%20angolo%20della%20pizza%20di%20Mapy!5e0!3m2!1sit!2sit!4v1704214393137!5m2!1sit!2sit" width="400" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
 
 </template>
