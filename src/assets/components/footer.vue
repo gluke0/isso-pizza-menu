07f2d376a884263ff4bd0e699c7a331706a994f8
gluke0 <milanesigianluca@gmail.com>
2024-01-02 12:07:41 +0100

    added credits to the footer

--- src/assets/components/footer.vue
+++ src/assets/components/footer.vue
@@ -11,10 +11,10 @@ export default {
   },
 }
 </script>
-3
+
 <template>
-  
-  <div class="justify-content-center d-flex">
+
+  <div class="justify-content-around d-flex">
     <div class="footerino">
       <div> <b><i>L'Angolo Della Pizza di Mapy </i></b> </div> 
       
@@ -23,6 +23,19 @@ export default {
       <div> <i> 24058 Isso BG </i> </div>
       
       <div> P. IVA 04604600165 </div>
+
+    </div>
+    <div class="credits">
+      <div>
+        <b><i>Made with <i class="fa-solid fa-laptop-code"></i> by:</i></b>
+      </div>
+      <div>
+        <i><a href="https://www.github.com/gluke0" target=”_blank” class="text-black text-decoration-none ms-4"> <i class="fa-brands fa-square-github"></i> gluke</a></i>
+      </div>
+      <br>
+      <div>
+      <i><a href="https://www.x.com/gluke0" target=”_blank” class="text-black text-decoration-none ms-4"><i class="fa-brands fa-square-x-twitter"></i> gluke</a></i>
+      </div>
     </div>
   </div>
 
@@ -32,6 +45,14 @@ export default {
 @import '../../style/main.scss';
 
 .footerino{
+  font-size: smaller
+}
+
+.credits{
   font-size: smaller;
+}
+
+#cci{
+  font: black!important;
 }
 </style>
