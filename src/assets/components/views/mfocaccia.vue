2010060a86d5d3e44d70129d8c7a39cc6be70735
gluke0 <milanesigianluca@gmail.com>
2024-01-03 09:47:37 +0100

    added focacce

--- src/assets/components/views/mfocaccia.vue
+++ src/assets/components/views/mfocaccia.vue
@@ -16,6 +16,31 @@ export default {
     
     <div>
       <h3 class="text-uppercase">focacce</h3>
+      <div class="d-flex justify-content-between">
+        <div>
+          <ul class="ms-5">
+            <li>Focaccia Pugliese</li>
+            <span>(origano e pomodorini)</span>
+            <li>Focaccia Patate</li>
+            <span>(patate e rosmarino)</span>
+            <li>Focaccia Zucchine e Grana</li>
+            <span>(zucchine e formaggio grana)</span>
+            <li>Focaccia Cipolle</li>
+            <span>(cipolle)</span>
+            <li>Focaccia Ripiena</li>
+            <span>(farcitura a piacimento con salumi tra: crudo, cotto e salame)</span>
+          </ul>
+        </div>
+        <div>
+          <ul class="me-5">
+            <li>€ 4,50 a por.</li>
+            <li>€ 4,50 a por.</li>
+            <li>€ 4,50 a por.</li>
+            <li>€ 4,50 a por.</li>
+            <li>€ 6,50 a por.</li>
+          </ul>
+        </div>
+      </div>
     </div>
 
 </template>
