d249689e4d82493100c3f4021d90328d6b53e0e9
gluke0 <milanesigianluca@gmail.com>
2024-01-03 14:33:05 +0100

    added pizzas

--- src/assets/components/views/mpizza.vue
+++ src/assets/components/views/mpizza.vue
@@ -20,15 +20,58 @@ export default {
         <div>
           <ul class="ms-5">
             <li>Pizza Margherita</li>
-            <span>(pomodoro e mozzarella)</span>
+            <span>(mozzarella, pomodoro)</span>
             <li>Pizza Rucola</li>
-            <span>(pomodoro, mozzarella, rucola)</span>
+            <span>(mozzarella, pomodoro, rucola)</span>
             <li>Pizza Cipolla</li>
-            <span>(pomodoro, mozzarella, cipolla)</span>
+            <span>(mozzarella, pomodoro, cipolla)</span>
             <li>Pizza Capperi</li>
-            <span>(pomodoro, mozzarella, capperi)</span>
+            <span>(mozzarella, pomodoro, capperi)</span>
             <li>Pizza Olive</li>
-            <span>(pomodoro, mozzarella, olive)</span>
+            <span>(mozzarella, pomodoro, olive)</span>
+            <li>Pizza Acciughe</li>
+            <span>(mozzarella, pomodoro, acciughe)</span>
+            <li>Pizza Carciofi</li>
+            <span>(mozzarella, pomodoro, carciofi)</span>
+            <li>Pizza Wurstel</li>
+            <span>(mozzarella, pomodoro, wurstel)</span>
+            <li>PIzza al Prosciutto Cotto</li>
+            <span>(mozzarella, pomodoro, prosciutto cotto)</span>
+            <li>Pizza Patate Fritte</li>
+            <span>(mozzarella, pomodoro, patate fritte)</span>
+            <li>Pizza Capricciosa</li>
+            <span>(mozzarella, pomodoro, olive, capperi, peperoni, funghi, carciofi)</span>
+            <li>Pizza Prosciutto e Fughi</li>
+            <span>(mozzarella, pomodoro, prosciutto cotto, funghi champignon)</span>
+            <li>Pizza Wurstel e Patatine</li>
+            <span>(mozzarella, pomodoro, wurstel, patatine fritte)</span>
+            <li>Pizza 4 Stagioni</li>
+            <span>(mozzarella, pomodoro, olive, capperi, funghi, peperoni, carciofi, prosciutto)</span>
+            <li>Pizza Pancetta e Tonno</li>
+            <span>(mozzarella, pomodoro, pancetta, tonno)</span>
+            <li>Pizza Mapy</li>
+            <span>(mozzarella, pomodoro, rucola, scaglie di grana, prosciutto crudo)</span>
+            <li>Pizza Tonno</li>
+            <span>(mozzarella, pomodoro, tonno)</span>
+            <li>Pizza Funghi Champignon</li>
+            <span>(mozzarella, pomodoro, funghi champignon)</span>
+            <li>Pizza Prosciutto Crudo</li>
+            <span>(mozzarella, pomodoro, prosciutto crudo)</span>
+            <li>Pizza Salame Piccante</li>
+            <span>(mozzarella, pomodoro, salame piccante)</span>
+            <li>Pizza Speck</li>
+            <span>(mozzarella, pomodoro, speck)</span>
+            <li>Pizza Salame</li>
+            <span>(mozzarella, pomodoro, salame)</span>
+            <li>Pizza Pancetta</li>
+            <span>(mozzarella, pomodoro, pancetta)</span>
+            <li>Pizza Verdure Grigliate</li>
+            <span>(mozzarella, pomodoro, verdure grigliate miste)</span>
+            <li>Pizza Funghi Misti</li>
+            <span>(mozzarella, pomodoro, funghi misti)</span>
+            <li>Pizza Tonno e Cipolla</li>
+            <span>(mozzarella, pomodoro, tonno, cipolla)</span>
+
           </ul>
         </div>
         <div>
