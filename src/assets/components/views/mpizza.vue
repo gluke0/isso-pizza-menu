c81c85e71b13870174ad5d66efd2dcef45cfec9d
gluke0 <milanesigianluca@gmail.com>
2024-01-03 14:14:07 +0100

    added some pizzas

--- src/assets/components/views/mpizza.vue
+++ src/assets/components/views/mpizza.vue
@@ -16,6 +16,35 @@ export default {
     
     <div>
       <h3 class="text-uppercase">pizze</h3>
+      <div class="d-flex justify-content-between">
+        <div>
+          <ul class="ms-5">
+            <li>Pizza Margherita</li>
+            <span>(pomodoro e mozzarella)</span>
+            <li>Pizza Rucola</li>
+            <span>(pomodoro, mozzarella, rucola)</span>
+            <li>Pizza Cipolla</li>
+            <span>(pomodoro, mozzarella, cipolla)</span>
+            <li>Pizza Capperi</li>
+            <span>(pomodoro, mozzarella, capperi)</span>
+            <li>Pizza Olive</li>
+            <span>(pomodoro, mozzarella, olive)</span>
+          </ul>
+        </div>
+        <div>
+          <ul class="me-5">
+            <li>€ 5,00 a por.</li>
+            <br>
+            <li>€ 5,50 a por.</li>
+            <br>
+            <li>€ 5,50 a por.</li>
+            <br>
+            <li>€ 5,50 a por.</li>
+            <br>
+            <li>€ 5,50 a por.</li>
+          </ul>
+        </div>
+      </div>
     </div>
 
 </template>
