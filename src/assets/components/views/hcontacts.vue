44708f9a40d0bb8ea83dbf4a23da5b9f85299d2d
gluke0 <milanesigianluca@gmail.com>
2024-01-08 10:53:02 +0100

    added maps link to contacts

--- src/assets/components/views/hcontacts.vue
+++ src/assets/components/views/hcontacts.vue
@@ -15,11 +15,15 @@ export default {
 <template>
     
     <div class="">
-      <div class="namep"> <h1>L'Angolo Della Pizza di Mapy </h1> </div> 
+      <div>
+        <a target="_blank" href="https://maps.app.goo.gl/GWR7UsDnFQ4gR2rbA">
+          <div class="namep"> <h1>L'Angolo Della Pizza di Mapy </h1> </div> 
       
-      <div>  <i> Via Cascina Secchi, 516 </i> </div>
+          <div>  <i> Via Cascina Secchi, 516 </i> </div>
       
-      <div> <i> 24058 Isso BG </i> </div>                 
+          <div> <i> 24058 Isso BG </i> </div> 
+        </a>
+      </div>                
 
       <br>
       <div class="calling"> <a target="_blank" class="text-black text-decoration-none" href="tel:+393895468163"><i> <b> <i class="fa-solid fa-phone"></i> +39 389 5468163 </b> </i> </a> </div>
@@ -35,6 +39,11 @@ export default {
 
 .calling:hover{
   transform: scale(1.03);
+}
+
+a{
+  text-decoration: none!important;
+  color: black!important;
 }
 
 </style>
